f3b10b2ecc7dd988fea2b14c21b739a91719a8eb
pawanchaudhari24 <pawanchaudhari4696>
2023-09-12 19:32:35 +0530

    sec commit

---
+++ dom.js
@@ -0,0 +1,14 @@
+//var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent = 'Hello';
+
+
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].style.fontWeight ='bold';
+items[2].style.backgroundColor ='green';
+items[0].style.fontWeight ='bold';
+items[1].style.fontWeight ='bold';
+items[3].style.fontWeight ='bold';
