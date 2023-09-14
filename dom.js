28836f2ad0adb2b71b8d3f4a597c79deeb279103
pawanchaudhari24 <pawanchaudhari4696>
2023-09-15 01:16:25 +0530

    Creating Nodes and Modifying Dom

--- dom.js
+++ dom.js
@@ -1,12 +1,49 @@
-//var headerTitle = document.getElementById('header-title');
-//console.log(headerTitle);
-//headerTitle.textContent = 'Hello';
+var itemList=document.querySelector('#items');
+//console.log(itemList.parentElement);
+//itemList.parentElement.style.backgroundColor='#f4f4f4';
+//console.log(itemList.parentElement.parentElement.parentElement)
+
+//child nodes
+//console.log(itemList.childnodes);
+
+//children nodes
+//console.log(itemList.children)
+//console.log(itemList.children[1])
+//itemlist.children[1].style.backgroundColor='yellow';
+
+//FirstChild
+//console.log(itemList.firstChild);
+//FirstElementChild
+//console.log(itemList.firstElementChild);
+//itemList.firstElementChild.textContent='Hello 1';
+//LastChild
+//console.log(itemList.lastChild);
+//LastElementChild
+//console.log(itemList.lastElementChild);
+//itemList.lastElementChild.textContent='Hello 4';
+//nextSibling
+//console.log(itemList.nextSibling);
+//nextElementSibling
+//console.log(itemList.nextElementSibling);
+//previousSibling
+//console.log(itemList.previousSibling);
+//PreviousElementSibling
+//console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.color='green';
+
+//Create Div
+
+var newDiv=document.createElement('div');
+newDiv.ClassName='hello ';
+newDiv.id='hello 1';
+newDiv.setAttribute('title','Hello Div');
+var newDivText=document.createTextNode('Hello World');
+newDiv.appendChild(newDivText);
+var container=document.querySelector('header.container');
+var h1=document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontsize='30px';
+container,insertBefore(newDiv,h1);
 
 
-
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-//console.log(items[2]);
-
-items[1].style.backgroundColor ='green';
 
